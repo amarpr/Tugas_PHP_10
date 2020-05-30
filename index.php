037dfb99ca941cd6328a913f921a0c76a98fca48
Muhamar <amararrasyid@gmail.com>
2020-05-30 21:00:04 +0700

    Tugas PHP 10
    Mengerjakan Tugas Niomic

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$arr = array(1,3,8,2,5,7,4,0);
+
+
+print("Sebelum Di Sorting : <br>");
+print_r($arr);
+
+
+sort($arr);
+print("<br> Setelah di sorting (Buble sort) : <br>");
+print_r($arr);
+
